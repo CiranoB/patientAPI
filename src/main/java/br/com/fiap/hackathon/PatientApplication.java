4053209db84a00e0e774e41566adf7f2e7dfb19c
CiranoB <ciranobelardony@gmail.com>
2025-02-10 16:28:51 +0000

    first commit

---
+++ src/main/java/br/com/fiap/hackathon/PatientApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientApplication {
+
+	public  static void main(String[] args) {
+		SpringApplication.run(PatientApplication.class, args);
+	}
+
+}
